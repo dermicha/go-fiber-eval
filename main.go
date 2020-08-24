e943cbdee8449df34e896d8590e17bb85cbeadc5
derMicha <dermicha@ubirch.com>
2020-08-24 23:28:08 +0200

    added database (sqlite3)

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"./book"
+	"./database"
+	"fmt"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
+
+func getFile(c *fiber.Ctx) {
+	err := c.SendFile("books.db")
+
+	if err != nil {
+		c.Next(err) // Pass error to Fiber
+	}
+}
 
 func helloWorld(c *fiber.Ctx) {
 	c.Send("Hello, World!")
@@ -10,10 +23,31 @@ func helloWorld(c *fiber.Ctx) {
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/", helloWorld)
+
+	app.Get("/file", getFile)
+
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
 }
 
 func main() {
 	app := fiber.New()
+
+	initDatabase()
+	defer database.DBConn.Close()
 
 	setupRoutes(app)
 	app.Listen(3000)
